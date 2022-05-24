3d34f3e04df37beeffd6c203368eb669e5848aaa
Estebanngarcia <fantingar@gmail.com>
2022-05-23 21:47:08 -0300

    fixed

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -5,7 +5,6 @@ const productsController = {
         res.render('createProduct.ejs')
     },
     editProduct: (req, res) => {
-<<<<<<< Updated upstream
         const product = data.find(items => items.id == req.params.idProduct)
         res.render('editProduct.ejs',{items:product})
     },
@@ -14,13 +13,8 @@ const productsController = {
     },
     editCongrats: (req, res) => {
         res.render('editProductCongrats.ejs')
-=======
-        res.render('editProduct.ejs')
     },
-    congrats: (req, res) => {
-        res.render('congrats.ejs')
->>>>>>> Stashed changes
-    },
+    
     detail: (req,res) =>{
         const product = data.find(items => items.id == req.params.idProduct)
         res.render('productDetail',{items:product})
