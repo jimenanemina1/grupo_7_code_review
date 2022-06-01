f5236518dcdb589c846f62a304c97f3659468d37
vicrocsal <victor.rocasalcedo@gmail.com>
2022-06-01 09:58:02 -0500

    added multer

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,4 +1,28 @@
 const express = require('express');
+const multer = require("multer");
+const path = require("path");
+
+// ************ Multer ************
+const normalizeWordsArray = (words) =>
+  words
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .split(" ");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../public/images"));
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `img-${normalizeWordsArray(req.body.name).join("-")}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+//************************* */
+
 
 
 const productsController = require('../controllers/productsController');
