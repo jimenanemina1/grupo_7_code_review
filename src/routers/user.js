9748718b3791cbd3cf9a5d3ddb773968de64f49c
vicrocsal <victor.rocasalcedo@gmail.com>
2022-06-18 15:38:54 -0500

    added validateImageUpload middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,6 +5,7 @@ const userController = require("../controllers/userController");
 
 const validations = require("../middlewares/validateRegisterForm");
 const upload = require("../middlewares/multer");
+const validateImageUpload = require("../middlewares/validateImageUpload");
 
 router.get("/login", userController.login);
 router.get("/register", userController.register);
@@ -12,6 +13,7 @@ router.post(
   "/register",
   upload.single("avatar"),
   validations,
+  validateImageUpload,
   userController.registerProcess
 );
 router.get("/:id", userController.profile);
