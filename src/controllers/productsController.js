3eb42e4aa38d7622426a704dd1afead089e7cca0
Estebanngarcia <fantingar@gmail.com>
2022-06-21 15:34:01 -0300

    logica de descuento

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -6,6 +6,7 @@ const path = require("path");
 
 const productsFilePath = path.join(__dirname, "../data/products.json");
 const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const productsController = {
     detail: (req, res) => {
@@ -15,6 +16,7 @@ const productsController = {
         })
     },
     list: (req, res) => {
+        const product = products.find(items => items.id == req.params.idProduct)
             // let productosMostrados = [];
             // products.forEach(element => {
             //     productosMostrados.push(element)
