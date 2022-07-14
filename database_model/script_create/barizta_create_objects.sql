4e02661f1863d64ede55c20822fc4a0dc6667530
gar46 <giolaver31@gmail.com>
2022-07-14 16:25:48 -0500

    archivo readme (inicializar proyecto)

---
+++ database_model/script_create/barizta_create_objects.sql
@@ -0,0 +1,108 @@
+﻿CREATE TABLE `categories2` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+CREATE TABLE  `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_id` int(11) NOT NULL,
+  `ammount` decimal(10,2) NOT NULL,
+  `billing_address` varchar(255) NOT NULL,
+  `shipping_address` varchar(255) NOT NULL,
+  `order_email` varchar(255) NOT NULL,
+  `order_date` datetime NOT NULL,
+  `status_orders_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_id` (`users_id`),
+  KEY `status_orders_id` (`status_orders_id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`status_orders_id`) REFERENCES `status_orders` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE  `order_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `orders_id` int(11) NOT NULL,
+  `products_id` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `orders_id` (`orders_id`),
+  KEY `products_id` (`products_id`),
+  CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE  `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `discount` int(11) DEFAULT NULL,
+  `size` int(11) NOT NULL,
+  `description` text,
+  `imgPath` varchar(100) DEFAULT NULL,
+  `create_date` datetime NOT NULL,
+  `stock` int(11) NOT NULL,
+  `categories_id` int(11) DEFAULT NULL,
+  `offer` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `categories_id` (`categories_id`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE  `products_review` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `products_id` int(11) NOT NULL,
+  `review_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `products_id` (`products_id`),
+  KEY `review_id` (`review_id`),
+  CONSTRAINT `products_review_ibfk_1` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `products_review_ibfk_2` FOREIGN KEY (`review_id`) REFERENCES `review` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE  `review` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `points` int(11) DEFAULT NULL,
+  `comment` text NOT NULL,
+  `users_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_id` (`users_id`),
+  CONSTRAINT `review_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE  `sequelizemeta` (
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`name`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE  `status_orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE  `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `imgPath` varchar(100) DEFAULT NULL,
+  `billing_address` varchar(255) NOT NULL,
+  `shipping_address` varchar(255) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+
+
