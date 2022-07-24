b03db5086f71f81d1912e4387ae45b6b6246ee5f
Jimena Nemiña <jimenanemina@Jimenas-MacBook-Pro.local>
2022-07-24 20:13:02 -0300

    added crud serch

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,17 +1,19 @@
 // const data = require("./data")
 
-const res = require("express/lib/response");
-const fs = require("fs");
+//const res = require("express/lib/response");
+//const fs = require("fs");
 const path = require("path");
 const db = require("../database/models");
+const {Op} = require("../../node_modules/sequelize");
 
-const productsFilePath = path.join(__dirname, "../data/products.json");
+//const productsFilePath = path.join(__dirname, "../data/products.json");
 //const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
-const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+//const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 
 const productsController = {
     detail: async(req, res) => {
+        console.log("se ejecuta detail")
         try{
         const productId = req.params.idProduct
         product = await db.Product.findByPk(productId)
@@ -27,15 +29,42 @@ const productsController = {
     }
     },
     list: async (req, res) => {
+        console.log("se ejecuta list")
+
         try{
-            db.Product.findAll()
-            .then(function(products){
-                return res.render("listProducts", { productosMostrados: products }) ;
+           product = await db.Product.findAll()
+            .then(function(product){
+                return  res.render('productDetail', {
+                    items: product
+                })
             })
         }  catch (error){
             console.log(error)
         }
         
- }
+ },
+ find: async (req, res) => {
+     console.log("me ejecuto")
+     let searchWord = req.params.searchWord;
+     console.log(searchWord)
+    try{
+        product = await db.Product.findOne({
+            where:{
+                name: {
+                    [Op.like]: `%${searchWord}%`
+                }
+            
+            }
+            
+        })
+        return  res.render('productDetail', {
+            items: product
+        })
+
+    }  catch (error){
+        console.log(error)
+    }
+    
+}
 }
 module.exports = productsController;
