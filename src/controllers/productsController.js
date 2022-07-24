1fa1932243b3a72949c899e635c0ebff05329048
Jimena Nemiña <jimenanemina@Jimenas-MacBook-Pro.local>
2022-07-24 16:36:53 -0300

    crud product detail

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -3,6 +3,7 @@
 const res = require("express/lib/response");
 const fs = require("fs");
 const path = require("path");
+const db = require("../database/models");
 
 const productsFilePath = path.join(__dirname, "../data/products.json");
 const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
@@ -10,12 +11,20 @@ const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 
 const productsController = {
-    detail: (req, res) => {
-        const product = products.find(items => items.id == req.params.idProduct)
+    detail: async(req, res) => {
+        try{
+        const productId = req.params.idProduct
+        product = await db.Product.findByPk(productId)
+        .then(function(product){
+            return product;
+        })
         console.log(JSON.stringify(product))
         res.render('productDetail', {
             items: product
         })
+    } catch (error){
+        console.log(error)
+    }
     },
     list: (req, res) => {
          res.render("listProducts", { productosMostrados: products }) ;
