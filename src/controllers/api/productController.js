5859c2a7a40c380a69796424bcee405aaaf87d09
Jimena Nemiña <jimenanemina@Jimenas-MacBook-Pro.local>
2022-08-24 18:19:25 -0300

    agregue logica de reviews a productos

--- src/controllers/api/productController.js
+++ src/controllers/api/productController.js
@@ -1,26 +1,80 @@
 const { send } = require("express/lib/response");
-let db = require("../../database/models")
+let db = require("../../database/models");
+const products = require("../data");
 
 module.exports = {
     
     listAll : async(req, res)  => {
         const limit = 10;
+        let productsWithReviewsArray = [];
+        let productsWithNoReviewsArray = [];
+
+        const url = "localhost:3001/api/products/";
         const offset = req.query.page && req.query.page > 0 ? req.query.page : 0;
+        let reviewsArray = [];
+        let review;
         try{
             totalProducts = await db.Product.findAndCountAll({
                 limit: limit,
                 offset: offset *limit,
                 attributes: ["id","name","description"],
-            }).then(({rows,count}) => {
+                }).then(({rows,count}) => {
+                    rows.forEach(async product => {
+                              try{
+                                productReviewObject = await db.Product_review.findOne({
+                                    where:{
+                                        products_id : product.id
+                                    }
+                                })
+                                    if(!productReviewObject){
+                                        console.log("product no existe en tabla product_reviews")
+                                        product = { 
+                                            id : product.id,
+                                            name: product.name,
+                                            description: product.description,
+                                            urlDetail: url + product.id,
+                                            reviews: "Este producto aun no tiene reviews"
+                                            } 
+                                            productsWithReviewsArray.push(product)
+
+                                    } else {
+                                        review = productReviewObject.review_id; 
+                                        
+                                    try{
+                                            productsReviews = await db.Review.findByPk(review)
+                                            .then((productReview) => {
+                                            reviewsArray.push(productReview.comment)
+                                                     })
+                                                    product = { 
+                                                        id : product.id,
+                                                        name: product.name,
+                                                        description: product.description,
+                                                        urlDetail: url + product.id,
+                                                        reviews: reviewsArray
+                                                        } 
+                                                 productsWithNoReviewsArray.push(product)
+
+                                    } catch (error){
+                                        console.log(error)
+                                    }
+                                }
+                                console.log("el array es")
+console.log(productsWithReviewsArray)
+                              } catch(error){
+                                  console.log(error)
+                              }
+                        
+                });
                 res.status(200).json({
                 count: count,
-                products: rows
-            })
-        })
-      } catch (error){
-         console.log(error)
-     }
- },
+                products: productsArray
+                 })
+                 
+           })
+            } catch (error){
+                console.log(error)
+            }
+    },
     detail : async(req, res) => {
         
         const productId = req.params.id;
@@ -29,8 +83,7 @@ module.exports = {
             product = await db.Product.findByPk(productId)
             .then((product) => {
                 
-                res.status(200).json({
-               // user: user
+            res.status(200).json({
                id : product.id,
                name: product.name,
                price: product.price,
@@ -40,7 +93,8 @@ module.exports = {
                imgPath: filePath + product.imgPath,
                stock: product.stock,
                categories_id: product.categories_id,
-               offer: product.offer
+               offer: product.offer,
+               reviews: "aca van las reviews de este producto"
             })
   })
 
@@ -49,5 +103,3 @@ module.exports = {
      }  
     }
 }
-
-
