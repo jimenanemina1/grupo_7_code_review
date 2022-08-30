6004846d189720e744d5c9202b1e95f159321971
Jimena Nemiña <jimenanemina@Jimenas-MacBook-Pro.local>
2022-08-29 22:13:59 -0300

    created seed orders

---
+++ src/database/seeders/20220828185821-insert_status_orders_table.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert('status_orders', [
+        {
+         name: "approved"
+        },
+        {
+         name: "reproved"
+        },
+        {
+         name: "suspended"
+        }
+   ],{});
+ 
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('status_orders', null, {});
+
+  }
+};
