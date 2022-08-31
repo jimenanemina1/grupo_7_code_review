7528ee2560f0aaa24e6f0cf14ff6be4f8e259a0c
Jimena Nemiña <jimenanemina@Jimenas-MacBook-Pro.local>
2022-08-31 10:45:52 -0300

    cleaned data

--- src/database/seeders/20220829185221-insert_orders_table.js
+++ src/database/seeders/20220829185221-insert_orders_table.js
@@ -15,8 +15,8 @@ module.exports = {
         {
          users_id: 1,
          ammount: 2000,
-         billing_address: 'mi calle inventada , 3984',
-         shipping_address: 'mi calle de mi suegra inventada , 3984',	
+         billing_address: 'Callao , 3984',
+         shipping_address: 'Fraga , 3984',	
          order_email:'correonuevo@gmail.com',
          order_date: new Date(),
          status_orders_id: 1
@@ -24,8 +24,8 @@ module.exports = {
         {
          users_id: 2,
          ammount: 4000,
-         billing_address: 'mi calle inventada , 3984',
-         shipping_address: 'mi calle de mi suegra inventada , 3984',	
+         billing_address: 'Cerro de pasco, 3984',
+         shipping_address: 'Pauner , 3984',	
          order_email:'otronuevo@gmail.com',
          order_date: new Date(),
          status_orders_id: 2
@@ -33,11 +33,29 @@ module.exports = {
         {
          users_id: 3,
          ammount: 500,
-         billing_address: 'mi otra calle inventada , 3984',
-         shipping_address: 'mi otra calle de mi suegra inventada , 3984',	
+         billing_address: 'España , 3984',
+         shipping_address: 'Malaver, 3984',	
          order_email:'pensaunonuevo@gmail.com',
          order_date: new Date(),
          status_orders_id: 3
+        },
+        {
+        users_id: 4,
+        ammount: 800,
+        billing_address: 'Olaguer y feliu , 3984',
+        shipping_address: 'Villate , 3984',	
+        order_email:'cambialounonuevo@gmail.com',
+        order_date: new Date(),
+        status_orders_id: 4
+        },
+        {
+        users_id: 5,
+        ammount: 9000,
+        billing_address: 'Ayacucho , 3984',
+        shipping_address: 'Malaver , 3984',	
+        order_email:'diferenteunonuevo@gmail.com',
+        order_date: new Date(),
+        status_orders_id: 5
         }
    ],{});
  
