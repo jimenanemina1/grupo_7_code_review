8d194cab997ba83d46cf4e0833f72ab24da08b0e
Jimena Nemiña <jimenanemina@Jimenas-MacBook-Pro.local>
2022-09-01 01:09:51 -0300

    added endpoint categories

--- src/controllers/api/categoryController.js
+++ src/controllers/api/categoryController.js
@@ -5,33 +5,19 @@ module.exports = {
     listAll : async(req, res)  => {
         const url = "localhost:3001/api/category/";
         const limit = 10;
-        let ordersArray = [];
-        let totalRevenue = 0;
+        let categoriesArray = [];
+      //  let totalRevenue = 0;
         const offset = req.query.page && req.query.page > 0 ? req.query.page : 0;
         try{
-            totalOrders = await db.Order.findAndCountAll({
+            totalOrders = await db.Category.findAndCountAll({
                 limit: limit,
                 offset: offset *limit,
-                attributes: ["id","users_id", "ammount","billing_address","shipping_address","order_email","order_date","status_orders_id"],
+                attributes: ["id","name"],
             }).then(({rows,count}) => {
-              rows.forEach(element => {
-                totalRevenue = totalRevenue + parseFloat(element.ammount);
-                 element = { 
-                    id : element.id,
-                    users_id: element.name,
-                    ammount: parseFloat(element.ammount),
-                    billing_address: url + element.id,
-                    shipping_address: element.shipping_address,
-                    order_email: element.order_email,
-                    order_date: element.order_date,
-                    status_orders_id: element.status_orders_id
-                   } 
-                   ordersArray.push(element)
-                });
+          
                 res.status(200).json({
                 count: count,
-                totalRevenue: totalRevenue,
-                orders: ordersArray
+                categories: rows
             })
         })
 
@@ -41,19 +27,23 @@ module.exports = {
  },
     detail : async(req, res) => {
         
-        const orderId = req.params.id;
+        const categoryId = req.params.id;
+        let count = 0;
         try{
-            user = await db.Order.findByPk(orderId)
-            .then((order) => {
+            category = await db.Category.findByPk(categoryId, {include: [{association: 'products'}]})
+            .then((category) => {
+                productsArray = category.products;
+                productsArray.forEach(element => {
+                    
+                      count = count + 1
+                   });
             res.status(200).json({
-               id : orderId,
-               users_id: order.users_id,
-               ammount : order.ammount,
-               billing_address: order.billing_address,
-               shipping_address: order.shipping_address,
-               order_email: order.order_email,
-               order_date: order.order_date,
-               status_orders_id: order.status_orders_id
+               id : categoryId,
+               name: category.name,
+               products: productsArray,
+               productsTotal: count
+               
+
                 })
   })
       } catch (error){
