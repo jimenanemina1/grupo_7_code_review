ebb2dce011d9025fd8b9b3fd5ac7f26a09dc2fbd
vicrocsal <victor.rocasalcedo@gmail.com>
2022-09-02 11:32:27 -0500

    Added userDropdown

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -37,7 +37,7 @@ const userController = {
 					res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 })
 				}
 
-        return res.redirect("/user/userProfile");
+        return res.redirect("/");
       }
       return res.render("login", {
         errors: {
